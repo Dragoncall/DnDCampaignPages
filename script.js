9cceb77d2408890497c287e2a4e1417c7b879ba3
Karel <kavdnbus.VandenBussche@ugent.be>
2019-02-13 13:44:26 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,180 @@
+const focusButtonClasses = ['animate-world', 'focus-khalesh', 'focus-eryltell', 'focus-azuros', 'focus-verzug', 'focus-mohram']
+const classNames = ['khalesh', 'eryltell', 'azuros', 'verzug']
+var worldInfo = {};
+let continentInfo = {};
+
+
+function getAnimationStyle(obj) {
+    return {
+        backgroundSize: obj.css('background-size'),
+        backgroundPositionX: obj.css('background-position-x'),
+        backgroundPositionY: obj.css('background-position-y')
+    }
+}
+
+function setAnimationStyle(obj, style) {
+    obj.css('background-size', style.backgroundSize)
+    obj.css('background-position-x', style.backgroundPositionX)
+    obj.css('background-position-y', style.backgroundPositionY)
+}
+
+function createAnimation(object, name) {
+    $.keyframe.define([{
+        name: 'to-' + name,
+        from: {},
+        to: {
+            'background-size': object.size,
+            'background-position-x': object.x,
+            'background-position-y': object.y
+        }
+    }]);
+}
+
+function doAnimation(jqueryObject, object, duration, name) {
+    createAnimation(object, name)
+    jqueryObject.playKeyframe({
+        name: 'to-' + name, // name of the keyframe you want to bind to the selected element
+        duration: duration + 'ms', // [optional, default: 0, in ms] how long you want it to last in milliseconds
+        timingFunction: 'steps(' + Math.floor((duration*30)/1000) + ')', // [optional, default: ease] specifies the speed curve of the animation
+        fillMode: 'forwards', //[optional, default: 'forward']  how to apply the styles outside the animation time, default value is forwards
+    });
+}
+
+function applyFocusClass(className, isCity) {
+    let obj = $('#top-hero')
+    obj.removeClass('animate-world')
+    setAnimationStyle(obj, getAnimationStyle(obj))
+    let overlay = $('#world-overlay')
+    overlay.removeClass('animate-world-overlay')
+    overlay.css('background-color', '#00000090')
+    if (isCity) {
+        doAnimation(obj, continentInfo[className], 1000, className)
+    } else {
+        doAnimation(obj, worldInfo[className], 500, className)
+    }
+}
+
+function setText(className, isCity) {
+    if (worldInfo === {} || (isCity && continentInfo === {}) ) {
+        console.log("Descriptions are empty...")
+        return
+    }
+    let info = worldInfo
+    if (isCity) {
+        info = continentInfo
+    }
+    let infoBlock = $('#info-block')
+    let descriptionBlock = $('#description')
+
+    infoBlock.html(info[className].description);
+    infoBlock.animate({ scrollTop: 0 }, "slow");
+
+    descriptionBlock.addClass('active-description')
+}
+
+function showCities(className) {
+    let subMenu = $('#sub-menu')
+    subMenu.html('')
+    cityNames = []
+    for (let name of Object.keys(worldInfo[className].cities)) {
+        let r= $('<button id="button-' + name + '" onclick="focusCity(\'' + name + '\')" class="continent-button subtitle is-block">' + name + '</button>');
+        subMenu.append(r);
+    }
+    continentInfo = worldInfo[className].cities
+}
+
+function setActive(className, isCity) {
+    let names = classNames
+    if (isCity) {
+        names = Object.keys(continentInfo)
+    }
+    for (let oldClassName of names) {
+        $('#button-' + oldClassName).removeClass('active-continent-button')
+    }
+    $('#button-' + className).addClass('active-continent-button')
+    $('#sub-menu').removeClass('is-hidden')
+}
+
+
+function onFocusButton(className) {
+    applyFocusClass(className, false)
+    setText(className, false)
+    setActive(className, false)
+    showCities(className)
+}
+
+function onFocusCityButton(className) {
+    applyFocusClass(className, true)
+    setText(className, true)
+    setActive(className, true)
+}
+
+
+function getWorldInfo() {
+    console.log('Getting Data')
+    $.ajax({
+        dataType: "json",
+        type: "get",
+        url: 'descriptions.json',
+        cache:false,
+        success: function(data){
+            worldInfo = data;
+            console.log(data)
+        },
+        error: function(XHR, status, error) {
+            console.log(status)
+            console.log(error)
+        }
+    });
+}
+
+// City Buttons
+
+function focusCity(name) {
+    onFocusCityButton(name)
+}
+
+// Continent Buttons
+
+function focusKhalesh() {
+    onFocusButton('khalesh')
+}
+
+function focusErylTell() {
+    onFocusButton('eryltell')
+}
+
+function focusAzuros() {
+    onFocusButton('azuros')
+}
+
+function focusVerZug() {
+    onFocusButton('verzug')
+}
+
+let isHidden = false;
+
+function hideOverlays() {
+    let obj = $('#world-overlay')
+    if (isHidden) {
+        obj.removeClass('is-hidden')
+    } else {
+        obj.addClass('is-hidden')
+    }
+    isHidden = !isHidden
+}
+
+function loadingDone() {
+    $('#loading').addClass('is-hidden')
+    $('#not-loading').removeClass('is-hidden')
+}
+
+$( document ).ready(function() {
+    getWorldInfo()
+    $.keyframe.define([]); // Init the keyframe shizzle
+    $('<img/>').attr('src', 'World.jpg').on('load', function() {
+        $(this).remove(); // prevent memory leaks as @benweet suggested
+        $('#top-hero').css('background-image', 'url("World.jpg")');
+        loadingDone()
+    });
+});
